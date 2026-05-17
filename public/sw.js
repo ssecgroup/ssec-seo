0810594c04ca1dfc82d4b6034847336371d3a37e
ssecgroup <ontech06@gmail.com>
2026-05-17 12:13:24 +0530

    Add PWA manifest, icons, and service worker

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+﻿// Simple service worker for PWA
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
